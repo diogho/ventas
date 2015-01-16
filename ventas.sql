feb3c9838cd1ff7214d9565aeca0e5840e154add
aricardolopez <aricardolopez@gmail.com>
2015-01-15 22:30:48 -0500

    Actualización BD

--- ventas.sql
+++ ventas.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 27-12-2014 a las 00:38:15
+-- Tiempo de generación: 16-01-2015 a las 04:29:18
 -- Versión del servidor: 5.6.20
 -- Versión de PHP: 5.5.15
 
@@ -19,38 +19,89 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `ventas`
 --
+CREATE DATABASE IF NOT EXISTS `ventas` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `ventas`;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbperfil`
+--
+
+DROP TABLE IF EXISTS `tbperfil`;
+CREATE TABLE IF NOT EXISTS `tbperfil` (
+`id` int(2) NOT NULL,
+  `nombre` varchar(10) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Volcado de datos para la tabla `tbperfil`
+--
+
+INSERT INTO `tbperfil` (`id`, `nombre`) VALUES
+(1, 'admin'),
+(2, 'vendedor'),
+(3, 'supervisor'),
+(4, 'inventario'),
+(5, 'logistica'),
+(6, 'contador');
+
+-- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `tbusuario`
 --
 
+DROP TABLE IF EXISTS `tbusuario`;
 CREATE TABLE IF NOT EXISTS `tbusuario` (
   `documento` varchar(10) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `telefono` varchar(50) NOT NULL,
-  `direccion` varchar(50) NOT NULL
+  `direccion` varchar(50) NOT NULL,
+  `idperfil` int(2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `tbusuario`
 --
 
-INSERT INTO `tbusuario` (`documento`, `nombre`, `telefono`, `direccion`) VALUES
-('1072661319', 'Wilmer Triana ', '3212891464', 'Calle 6 # 14 - 69'),
-('1072665508', 'Andres', '8638607', 'Cra'),
-('20505607', 'Ricardo Suarez', '3206781375', 'Cra 79 # 19 - 08'),
-('89120', 'test pass', '6712', 'Cra 1');
+INSERT INTO `tbusuario` (`documento`, `nombre`, `telefono`, `direccion`, `idperfil`) VALUES
+('1072665508', 'Andres', '8638608', 'Cra', 2);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `tbperfil`
+--
+ALTER TABLE `tbperfil`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `tbusuario`
 --
 ALTER TABLE `tbusuario`
- ADD PRIMARY KEY (`documento`);
+ ADD PRIMARY KEY (`nombre`), ADD KEY `idperfil` (`idperfil`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbperfil`
+--
+ALTER TABLE `tbperfil`
+MODIFY `id` int(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tbusuario`
+--
+ALTER TABLE `tbusuario`
+ADD CONSTRAINT `tbusuario_ibfk_1` FOREIGN KEY (`idperfil`) REFERENCES `tbperfil` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
